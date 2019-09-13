fcdb9a4cc5cd814b2a577ef517f9b001dd48c52f
Tony <ScoffStudios@gmail.com>
2019-09-12 23:01:00 -0500

    Linked up some projects

--- my-app/src/Components/projects.js
+++ my-app/src/Components/projects.js
@@ -10,27 +10,65 @@ class Projects extends Component{
 	}
 
 toggleCategories(){
-	if(this.state.activeTab === 0){ {/* Unreal Projects*/}
+	if(this.state.activeTab === 0){ {/* Game Projects*/}
 		return(
 			<div className="projects-grid">
 				{/* Project 1 */}
 				<Card shadow={5} style={{minWidth: '450',margin: 'auto'}}>
-					<CardTitle style ={{color: '#237A57',align: 'center', height: '176px', background: 'url(https://www.sccpre.cat/mypng/detail/18-189103_unreal-engine-logo-png-png-download-transparent-unreal.png) center / cover'}} > Unreal Project #1</CardTitle>
+					<CardTitle style ={{color: 'white',align: 'center', height: '176px', background: 'url(https://img.itch.zone/aW1hZ2UvMzE3MDc2LzE1NjE2MDMucG5n/original/pN%2BEPb.png) center / cover'}} >Fields of Flux</CardTitle>
 					<CardText>
-						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+						Game Jam Project for "Chillennium Game Jam" 2018. Won best in design. I did the effects programming, such as the bloom and grapple mechanics. Mainly acted as design lead.
 					</CardText>
 					<CardActions border>
-						<Button colored>GitHub</Button>
-						<Button colored>Itch</Button>
+						<a href="https://github.com/Tcarr20/FliedsOfFlux" rel="noopener noreferrer" target="_blank">
+						<Button colored ripple>GitHub </Button>
+						</a>
+						<a href="https://idomybest.itch.io/fields-of-fluxx" rel="noopener noreferrer" target="_blank">
+						<Button colored ripple>Itch.io</Button>
+						</a>
 					</CardActions>
-					<CardMenu style = {{color: '#237A57'}}>
-						<IconButton name="share"/>
-					</CardMenu>
 				</Card>
 
 				{/* Project 2 */}
 				<Card shadow={5} style={{minWidth: '450',margin: 'auto'}}>
-					<CardTitle style ={{color: '#237A57',align: 'center', height: '176px', background: 'url(https://www.sccpre.cat/mypng/detail/18-189103_unreal-engine-logo-png-png-download-transparent-unreal.png) center / cover'}} > Unreal Project #1</CardTitle>
+					<CardTitle style ={{color: 'white',align: 'center', height: '176px', background: 'url(https://img.itch.zone/aW1hZ2UvNDQ2NTQ4LzIyNTQ1ODYuZ2lm/347x500/XoqNGE.gif) center / cover'}} >Fields of Flux</CardTitle>
+					<CardText>
+						Built in Unreal Engine 4, for my Game Programming 4250 course at UNT. I built a gravity defying Peer-to-Peer network for rolling around and shooting each other.
+					</CardText>
+					<CardActions border>
+						<a href="https://github.com/Tcarr20/BallPit" rel="noopener noreferrer" target="_blank">
+						<Button colored ripple>GitHub </Button>
+						</a>
+						<a href="https://idomybest.itch.io/ballpit" rel="noopener noreferrer" target="_blank">
+						<Button colored ripple>Itch.io</Button>
+						</a>
+					</CardActions>
+				</Card>
+
+				{/* Project 3 */}
+				<Card shadow={5} style={{minWidth: '450',margin: 'auto'}}>
+					<CardTitle style ={{color: 'white',align: 'center', height: '176px', background: 'url(https://img.itch.zone/aW1hZ2UvMTg0MjIwLzg2MTk1Ni5wbmc=/347x500/vWygpb.png) center / cover'}} >Good Cop Rad Cop</CardTitle>
+					<CardText>
+						Game Jam Project for "Chillennium Game Jam" 2017. I was the lead programmer for this game. I created simple AI, and procedural generation of obstacles.
+					</CardText>
+					<CardActions border>
+						<a href="https://certly.itch.io/good-cop-rad-cop" rel="noopener noreferrer" target="_blank">
+						<Button colored ripple>Itch.io</Button>
+						</a>
+					</CardActions>
+				</Card>
+
+				</div>
+
+
+		)
+
+	}else if (this.state.activeTab === 1) { {/* other Projects*/}
+		return(
+			<div className="projects-grid">
+				{/* Project 1 */}
+				<Card shadow={5} style={{minWidth: '450',margin: 'auto'}}>
+					<CardTitle style ={{color: '#237A57',align: 'center', height: '176px', background: 'url(https://pbs.twimg.com/media/D_yc0hcW4BAcxY1.png) center / cover'}} > Other Project</CardTitle>
 					<CardText>
 						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
 					</CardText>
@@ -44,46 +82,7 @@ toggleCategories(){
 				</Card>
 			</div>
 		)
-
-	}else if (this.state.activeTab === 1) { {/* GameMaker Projects*/}
-		return(
-			<div className="projects-grid">
-				{/* Project 1 */}
-				<Card shadow={5} style={{minWidth: '450',margin: 'auto'}}>
-					<CardTitle style ={{color: '#237A57',align: 'center', height: '176px', background: 'url(https://www.sccpre.cat/mypng/detail/18-189103_unreal-engine-logo-png-png-download-transparent-unreal.png) center / cover'}} > Unreal Project #1</CardTitle>
-					<CardText>
-						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-					</CardText>
-					<CardActions border>
-						<Button colored>GitHub</Button>
-						<Button colored>Itch</Button>
-					</CardActions>
-					<CardMenu style = {{color: '#237A57'}}>
-						<IconButton name="share"/>
-					</CardMenu>
-				</Card>
-
-				{/* Project 2 */}
-				<Card shadow={5} style={{minWidth: '450',margin: 'auto'}}>
-					<CardTitle style ={{color: '#237A57',align: 'center', height: '176px', background: 'url(https://www.sccpre.cat/mypng/detail/18-189103_unreal-engine-logo-png-png-download-transparent-unreal.png) center / cover'}} > Unreal Project #1</CardTitle>
-					<CardText>
-						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-					</CardText>
-					<CardActions border>
-						<Button colored>GitHub</Button>
-						<Button colored>Itch</Button>
-					</CardActions>
-					<CardMenu style = {{color: '#237A57'}}>
-						<IconButton name="share"/>
-					</CardMenu>
-				</Card>
-			</div>
-		)
-	}else if (this.state.activeTab === 2) { {/* Other Projects*/}
-		return(
-			<div><h1>This is Other</h1></div>
-		)
-	}else if (this.state.activeTab === 3) { {/* React Projects*/}
+	}else if (this.state.activeTab === 2) { {/* React Projects*/}
 		return(
 			<div className= "landing-div"style = {{width: '100%',height: '100%',margin: 'auto'}}>
 
@@ -117,8 +116,7 @@ toggleCategories(){
 		return(
 			<div className="category-tabs">
 				 <Tabs activeTab ={this.state.activeTab} onChange={(tabId)=> this.setState({activeTab: tabId})} ripple>
-					 	<Tab>Unreal Enigne</Tab>
-					 	<Tab>GameMaker Studio</Tab>
+					 	<Tab>Games</Tab>
 					 	<Tab>Other</Tab>
 					 	<Tab>React</Tab>
 				 </Tabs>
